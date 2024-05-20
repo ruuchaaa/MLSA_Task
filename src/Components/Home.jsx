7a5eeda015bfcf075e74beccc50e4c39aa59faf6
rucha <rucha.chormunge@studentambassadors.com>
2024-05-20 17:06:33 +0530

    Add Changes 2

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,9 +20,9 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/src/images/mlsa.jpg";
+import image from "../images/mlsa.jpg";
 
-const imageAltText = "Rucha Chormunge - Web Developer and AI-ML Enthusiast.";
+const imageAltText = "Rucha Chormunge Web Developer and AI-ML Enthusiast.";
 
 const Home = ({ name, title }) => {
   return (
